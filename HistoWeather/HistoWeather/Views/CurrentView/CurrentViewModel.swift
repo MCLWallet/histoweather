7dd52d1e2cc7f502adf6cec375c3f97504af2486
milostustojiljkovic <101019284+milostustojiljkovic@users.noreply.github.com>
2022-12-03 12:24:38 +0100

    #2 Repository and Persistance CurrentViewModel

---
+++ HistoWeather/HistoWeather/Views/CurrentView/CurrentViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  CurrentViewModel.swift
+//  HistoWeather
+//
+//  Created by Milos Stojiljkovic on 02.12.22.
+//
+
+import Foundation
+
+struct CurrentViewModel {
+
+    private let dayWeatherRepository: DayWeatherRepository
+
+    init(dayWeatherRepository: DayWeatherRepository = DayWeatherRepository()) {
+        self.dayWeatherRepository = dayWeatherRepository
+    }
+
+    func fetchapi() async {
+        dayWeatherRepository.loadData()
+        print("fetchapi")
+    }
+}
