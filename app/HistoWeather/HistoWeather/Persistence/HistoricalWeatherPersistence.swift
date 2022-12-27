186b21c4419b0d6786f8d1e34b6bacb3f7e053ac
Milos Stojiljkovic <milos@Miloss-MacBook-Pro.local>
2022-12-27 17:28:53 +0100

    persistence working

--- app/HistoWeather/HistoWeather/Persistence/HistoricalWeatherPersistence.swift
+++ app/HistoWeather/HistoWeather/Persistence/HistoricalWeatherPersistence.swift
@@ -4,42 +4,37 @@
 //
 //  Created by Marcell Lanczos on 23.12.22.
 //
-
+//
 import Foundation
 import CoreData
 
 struct HistoricalWeatherPersistence {
 	private let context = PersistenceController.shared.backgroundContext
-	
+
 	static func fetchHistoricalWeather() -> NSFetchRequest<HistoricalWeather> {
 		let request = HistoricalWeather.fetchRequest()
-// print("Lat:\(Coordinates.coordinate.latitude)   Long:\(Coordinates.coordinate.longitude)")
-// request.predicate = NSPredicate(format:"latitude == %d AND longitude == %d",
-//        Coordinates.latitude, Coordinates.longitude)
 		request.sortDescriptors = []
 		return request
 	}
-	
+
 	static func fetchAllHistoricalWeather() -> NSFetchRequest<HistoricalWeather> {
 		let request = HistoricalWeather.fetchRequest()
 		request.sortDescriptors = []
 		return request
 	}
-	
+
 	static func fetchHistoricalDaily() -> NSFetchRequest<HistoricalDaily> {
 		let request = HistoricalDaily.fetchRequest()
-//       request.predicate = NSPredicate(format: "dayweather.latitude == %d AND dayweather.longitude == %d",
-//        Coordinates.coordinate.latitude, Coordinates.coordinate.longitude)
 		request.sortDescriptors = [NSSortDescriptor(key: "time", ascending: true)]
 		return request
 	}
-	
-	func addHistoricalDaily(from historicalWeather: HistoricalWeather, city: String, country: String) async {
+
+	func addHistoricalWeather(from historicalWeatherDecodable: HistoricalWeatherDecodable, city: String, country: String) async {
 		await context.perform {
-			_ = historicalWeather(weather: historicalWeather, city: city, country: country, context: context) }
+            _ = HistoricalWeather(historicalWeather: historicalWeatherDecodable, city: city, country: country, context: context) }
 		context.saveContext()
 	}
-	
+
 	func removeAllFriends() async throws {
 		try await context.perform {
 			try context.fetch(HistoricalWeatherPersistence.fetchAllHistoricalWeather()).forEach {
@@ -50,29 +45,37 @@ struct HistoricalWeatherPersistence {
 	}
 }
 
-extension HistoricalWeather {
-	convenience init(historicalWeather: HistoricalWeather, city: String, country: String, context: NSManagedObjectContext) {
-		self.init(context: context)
-		self.city = city
-		self.country = country
-		for i in 0...(historicalWeather.daily.temperature_2m_max.count - 1) {
-			addToDay(Day(day: DayEntry(time: convertDate(date: historicalWeather.daily.time[i]),
-							  weathericoncode: weatherCodeToIcon(weatherCode: historicalWeather.daily.weathercode[i]),
-							  temperature_2m_max: historicalWeather.daily.temperature_2m_max[i],
-							  temperature_2m_min: historicalWeather.daily.temperature_2m_min[i]
-							 ),
-						 context: context))
-		}
-	}
+extension HistoricalDaily {
+    convenience init(day: HistoricalDailyEntry,
+                     context: NSManagedObjectContext) {
+        self.init(context: context)
+        self.time = day.time
+        self.weathericoncode = day.weathericoncode
+        self.temperature_2m_max = day.temperature_2m_max
+        self.temperature_2m_min = day.temperature_2m_min
+    }
 }
 
-extension HistoricalDaily {
-	convenience init(day: DayEntry,
-					 context: NSManagedObjectContext) {
-		self.init(context: context)
-		self.time = day.time
-		self.weathericoncode = day.weathericoncode
-		self.temperature_2m_max = day.temperature_2m_max
-		self.temperature_2m_min = day.temperature_2m_min
-	}
+extension HistoricalWeather {
+    convenience init(historicalWeather: HistoricalWeatherDecodable, city: String, country: String, context: NSManagedObjectContext) {
+        self.init(context: context)
+        self.city = city
+        self.country = country
+        for i in 0...(historicalWeather.daily.temperature_2m_max.count - 1) {
+            
+            addToHistoricalDaily(HistoricalDaily(day: HistoricalDailyEntry(
+                time: convertDate(date: historicalWeather.daily.time[i]),
+                weathericoncode: weatherCodeToIcon(weatherCode: historicalWeather.daily.weathercode[i]),
+                              temperature_2m_max: historicalWeather.daily.temperature_2m_max[i],
+                              temperature_2m_min: historicalWeather.daily.temperature_2m_min[i]
+                             ), context: context))
+        }
+    }
+}
+
+struct HistoricalDailyEntry {
+    let time: Date
+    let weathericoncode: String
+    let temperature_2m_max: Double
+    let temperature_2m_min: Double
 }
